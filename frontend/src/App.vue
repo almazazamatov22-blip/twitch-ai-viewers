74683c1bbcd0e8d54716418463978fa800df6705
almazazamatov222-cmyk <almazazamatov222@gmail.com>
2026-04-13 04:13:12 +0500

    Revert "add: learn channel input in UI"
    This reverts commit 25e059b8fff514a34a3769d3b835d2b047f87b95.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -44,15 +44,6 @@ const transcriptHistory = reactive([])
 const learning = ref(false)
 const learnStats = reactive({ messages: 0, words: 0 })
 const learnLogs = reactive([])
-const learnChannel = ref('')
-const learnBotCount = ref(0)
-const learnTranscriptDuration = ref(15)
-
-function setLearnChannel() {
-  if (!learnChannel.value) return
-  socket.value.emit('learn:setChannel', { channel: learnChannel.value })
-  learnLogs.push('Канал изменён на ' + learnChannel.value)
-}
 
 function startLearning() {
   learning.value = true
@@ -193,11 +184,6 @@ onMounted(() => {
     learning.value = data.running
     learnStats.messages = data.messages
     learnStats.words = data.words
-  })
-  
-  s.on('learn:config', data => {
-    if (data.channel) learnChannel.value = data.channel
-    if (data.tokens) learnBotCount.value = data.tokens
   })
   
   s.on('learn:log', msg => {
@@ -473,16 +459,9 @@ const themeOverrides = {
                   </div>
                   
                   <n-card title="Настройка обучения">
-                    <n-space vertical>
-                      <n-input-group>
-                        <n-input-group-label>Канал:</n-input-group-label>
-                        <n-input v-model:value="learnChannel" placeholder="baz1221" style="width: 200px" />
-                        <n-button type="primary" @click="setLearnChannel">Сохранить</n-button>
-                      </n-input-group>
-                      <n-alert type="info" :caption="'Текущий канал: ' + learnChannel">
-                        Боты: {{ learnBotCount }} | Транскрипция: {{ learnTranscriptDuration }}с
-                      </n-alert>
-                    </n-space>
+                    <n-alert type="warning">
+                      Настройки берутся из Variables: LEARN_CHANNEL и LEARN_OAUTH
+                    </n-alert>
                     <n-divider />
                     <n-space>
                       <n-button type="primary" @click="startLearning" :disabled="learning">Старт</n-button>
